7e765340b8ea76deca4e0f8d75060c553102f56f
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-11-01 05:54:50 +0700

    Add verify() function.

--- src/shared/token.ts
+++ src/shared/token.ts
@@ -1,3 +1,4 @@
+import { APIGatewayProxyEvent } from 'aws-lambda';
 import jwt from 'jsonwebtoken';
 
 const refreshSecret = process.env.JWT_REFRESH_SECRET;
@@ -12,5 +13,27 @@ export const makeAccessToken = (payload: string | object | Buffer) => {
 export const makeRefreshToken = (payload: string | object | Buffer) => {
 	return jwt.sign(payload, refreshSecret, {
 		expiresIn: '30d'
+	});
+}
+
+export const verify = async (event: APIGatewayProxyEvent) => {
+	return new Promise<[jwt.VerifyErrors | null, string | jwt.JwtPayload | undefined]>(resolve => {
+		const header = event.headers['Authorization'];
+		if (!header) {
+			resolve([new jwt.JsonWebTokenError("Missing Authorization header."), undefined]);
+			return;
+		}
+		const splitted = header.split(' ', 2);
+		if (splitted.length !== 2 || splitted[0].toLowerCase() !== 'bearer') {
+			resolve([new jwt.JsonWebTokenError("Missing Bearer token."), undefined]);
+			return;
+		}
+
+		jwt.verify(splitted[1], accessSecret, (err, decoded) => {
+			if (err) {
+				console.log(err);
+			}
+			resolve([err, decoded]);
+		});
 	});
 }
