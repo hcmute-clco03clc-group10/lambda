6eb06dbca88382046af873f5d26bbdb61d6b272b
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-11-01 05:55:19 +0700

    Update tokenFunction handler using shared verify().

--- src/tokenFunction/index.ts
+++ src/tokenFunction/index.ts
@@ -1,48 +1,27 @@
-import jwt from 'jsonwebtoken';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { makeAccessToken } from '../shared/token';
+import { makeAccessToken, verify } from '../shared/token';
 
-const refreshSecret = process.env.JWT_REFRESH_SECRET;
-
-const GET = (event: APIGatewayProxyEvent): APIGatewayProxyResult => {
-	const cookie = (event.headers.Cookie || '')
-		.split(',')
-		.reduce((acc, v) => {
-			const splitted = v.split('=');
-			acc[splitted[0]] = splitted[1];
-			return acc;
-		}, {} as { [key: string]: string | undefined });
-
-	if (!cookie.refreshToken) {
+const GET = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+	const [err, decoded] = await verify(event);
+	if (err) {
 		return {
-			statusCode: 403,
-			body: ''
+			statusCode: 400,
+			body: err.message
 		}
 	}
-
-	const refreshToken = cookie.refreshToken;
-	const payload = jwt.verify(refreshToken, refreshSecret);
-	if (!payload) {
-		return {
-			statusCode: 403,
-			body: ''
-		}
-	}
-
 	return {
 		statusCode: 200,
-		body: JSON.stringify({
-			message: 'hello world',
-		}),
+		body: 'New access token granted.',
 		headers: {
-			'Set-Cookie': `accessToken=${makeAccessToken(payload)}`,
-			'httpOnly': true,
-			'secure': true,
+			'Set-Cookie': `accessToken=${makeAccessToken(decoded!)}`,
+			'HttpOnly': true,
+			'Secure': true,
+			'Path': '/'
 		}
 	};
 }
 
-export const lambdaHandler = async (
+export const handler = async (
 	event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
 	if (event.httpMethod === 'GET') {
