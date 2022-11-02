3e4962d1b0aa492b4fab244a7701e02a47ad79b9
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-11-03 04:32:57 +0700

    Update put().

--- src/shared/dynamodb/users.ts
+++ src/shared/dynamodb/users.ts
@@ -1,15 +1,21 @@
+import crypto from 'crypto';
 import { makeHash, makeSalt } from '../pbkdf2';
 import { ddc } from '.';
 
-export const put = async (username: string, password: string) => {
+export const put = async (username: string, email: string, password: string) => {
 	const salt = makeSalt();
+	const [err, hash] = await makeHash(password, salt);
+	if (err) {
+		return err;
+	}
 	return ddc.put({
 		TableName: 'users',
 		Item: {
 			_id: crypto.randomUUID(),
 			username,
+			email,
 			salt,
-			password: await makeHash(password, salt)
+			password: hash,
 		}
 	}).promise();
 }
