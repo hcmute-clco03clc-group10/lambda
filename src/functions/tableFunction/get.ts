4aa1543f781a3cad2b2a02fc94ff8f8b8e191cf6
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-11-04 14:51:37 +0700

    Implement /table GET handler.

---
+++ src/functions/tableFunction/get.ts
@@ -0,0 +1,45 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { verifyAccessTokenOrResign } from 'shared/token';
+import { ddc } from 'shared/dynamodb';
+
+export const GET = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+	const tableName = event.pathParameters?.tableName;
+	if (!tableName) {
+		return {
+			statusCode: 400,
+			body: 'Missing table name.'
+		};
+	}
+
+	let [err, decoded, setCookie] = await verifyAccessTokenOrResign(event);
+	if (err) {
+		return {
+			statusCode: 403,
+			body: 'Unauthorized.'
+		}
+	}
+
+	const res = await ddc.get({
+		TableName: 'users',
+		Key: {
+			id: decoded.id,
+			username: decoded.username
+		},
+		ProjectionExpression: 'tables'
+	}).promise();
+
+	if(res.$response.error) {
+		return {
+			statusCode: 400,
+			body: res.$response.error.message
+		}
+	}
+	
+	const tables = res.Item!.tables?.values || [] as string[];
+	return {
+		statusCode: 200,
+		body: JSON.stringify(tables),
+		headers: Object.assign({}, setCookie)
+	}
+}
+
--- src/functions/tableFunction/index.ts
+++ src/functions/tableFunction/index.ts
@@ -1,10 +1,14 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { PUT } from './put';
+import { GET } from './get';
 
 export const handler = async (
 	event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
 	switch(event.httpMethod.toUpperCase()) {
+		case 'GET': {
+			return GET(event);
+		}
 		case 'PUT': {
 			return PUT(event);
 		}
