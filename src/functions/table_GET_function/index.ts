101a9a62fc8eadaf45af9a4fc2892c77a0425d18
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-11-05 04:18:50 +0700

    Implement /table GET handler.

---
+++ src/functions/table_GET_function/index.ts
@@ -0,0 +1,44 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { verifyAccessTokenOrResign } from 'shared/token';
+import { ddc } from 'shared/dynamodb';
+
+export const GET = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+	let [err, decoded, setCookie] = await verifyAccessTokenOrResign(event);
+	if (err) {
+		return {
+			statusCode: 403,
+			body: 'Unauthorized.'
+		}
+	}
+
+	const res = await ddc.get({
+		TableName: 'users',
+		Key: {
+			id: decoded.id,
+			username: decoded.username
+		},
+		ProjectionExpression: 'tables'
+	}).promise();
+
+	if(res.$response.error) {
+		return {
+			statusCode: 400,
+			body: res.$response.error.message
+		}
+	}
+	
+	const tables = res.Item!.tables?.values || [] as string[];
+	return {
+		statusCode: 200,
+		body: JSON.stringify(tables),
+		headers: Object.assign({
+			'Content-Type': 'application/json'
+		}, setCookie)
+	}
+}
+
+export const handler = async (
+	event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+	return GET(event);
+};
