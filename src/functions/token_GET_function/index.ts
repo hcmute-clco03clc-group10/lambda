91a169e906a0fd6a2f0b344198f04d24e938a0af
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-08 20:38:17 +0700

    Add SameSite=None; Secure to refreshed cookie.

--- src/functions/token_GET_function/index.ts
+++ src/functions/token_GET_function/index.ts
@@ -1,20 +1,30 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { makeProjectedPayload } from 'shared/payload';
-import { extractToken, makeAccessToken, verifyRefreshToken } from 'shared/token';
+import {
+	extractToken,
+	makeAccessToken,
+	verifyRefreshToken,
+} from 'shared/token';
 import * as http from 'shared/http';
 
-const GET = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-	const [err, decoded] = await verifyRefreshToken(extractToken(event, 'refreshToken'));
+const GET = async (
+	event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+	const [err, decoded] = await verifyRefreshToken(
+		extractToken(event, 'refreshToken')
+	);
 	if (err) {
 		return http.respond.error(400, err);
 	}
-	return http.respond.text(200, 'New access token granted.', {
-		'Set-Cookie': `accessToken=${makeAccessToken(makeProjectedPayload(decoded))}`,
-		'HttpOnly': true,
-		'Secure': true,
-		'Path': '/'
+	return http.respond.json(200, decoded, {
+		'Set-Cookie': `accessToken=${makeAccessToken(
+			makeProjectedPayload(decoded)
+		)}; SameSite=None; Secure`,
+		HttpOnly: true,
+		Secure: true,
+		Path: '/',
 	});
-}
+};
 
 export const handler = async (
 	event: APIGatewayProxyEvent
