1e87330f4f998055a14e22218d841739fea38396
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-10 01:42:52 +0700

    Add OPTIONS_function.

---
+++ src/functions/OPTIONS_function/index.ts
@@ -0,0 +1,6 @@
+import { APIGatewayProxyResult } from 'aws-lambda';
+import * as http from 'shared/http';
+
+export const handler = async (): Promise<APIGatewayProxyResult> => {
+	return http.respond.text(200, '');
+};
