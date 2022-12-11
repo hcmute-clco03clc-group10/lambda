e65007689ff6e07456dbeb4759631260523604ea
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-12 04:12:37 +0700

    Add name query string parameter.

--- src/functions/table_GET_function/index.ts
+++ src/functions/table_GET_function/index.ts
@@ -6,6 +6,53 @@ import * as http from 'shared/http';
 export const GET = async (
 	event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
+	const name = event.queryStringParameters?.name;
+	if (name) {
+		return getOne(event, name);
+	}
+	return getAll(event);
+};
+
+const getOne = async (event: APIGatewayProxyEvent, name: string) => {
+	const [err, decoded, setCookie] = await verifyAccessTokenOrResign(event);
+	if (err) {
+		return http.respond(event).unauthorized();
+	}
+	const res = await ddc
+		.get({
+			TableName: 'users',
+			Key: { id: decoded.id, email: decoded.email },
+			ProjectionExpression: 'tables',
+		})
+		.promise();
+	if (res.$response.error) {
+		return http.respond(event).error(400, res.$response.error, setCookie);
+	}
+	const tables = res.Item!.tables?.values as string[];
+	if (!tables || !tables.includes(name)) {
+		return http.respond(event).json(200, {}, setCookie);
+	}
+
+	const result = await ddb
+		.describeTable({
+			TableName: `${decoded.id}_${name}`,
+		})
+		.promise();
+	if (result.$response.error) {
+		return http.respond(event).error(502, result.$response.error);
+	}
+	const table = result.Table;
+	if (!table) {
+		return http.respond(event).json(200, {}, setCookie);
+	}
+
+	table.TableName = table.TableName!.substring(
+		table.TableName!.indexOf('_') + 1
+	);
+	return http.respond(event).json(200, table, setCookie);
+};
+
+const getAll = async (event: APIGatewayProxyEvent) => {
 	const [err, decoded, setCookie] = await verifyAccessTokenOrResign(event);
 	if (err) {
 		return http.respond(event).unauthorized();
