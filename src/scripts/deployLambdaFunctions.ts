8de603db00deee932c9277ecadd86a6ec26756fc
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-12 04:15:13 +0700

    Remove brackets from function name before uploading.

--- src/scripts/deployLambdaFunctions.ts
+++ src/scripts/deployLambdaFunctions.ts
@@ -6,7 +6,7 @@ import fs from 'fs';
 const dist = path.resolve('dist/functions');
 const argv = process.argv.slice(2);
 const dirs = argv.length
-	? globDirs(dist).filter(dir => argv.includes(path.basename(dir)))
+	? globDirs(dist).filter((dir) => argv.includes(path.basename(dir)))
 	: globDirs(dist);
 
 for (const dir of dirs) {
@@ -16,16 +16,20 @@ for (const dir of dirs) {
 			console.log(`> ${basename}: ${err.message}`);
 			return;
 		}
-		lambda.updateFunctionCode({
-			FunctionName: basename,
-			ZipFile: data
-		}, (err, data) => {
-			if (err) {
-				console.log(`> ${basename}: ${err.message}`);
-				return;
+		lambda.updateFunctionCode(
+			{
+				FunctionName: basename.replace('{', '').replace('}', ''),
+				ZipFile: data,
+			},
+			(err, data) => {
+				if (err) {
+					console.log(`> ${basename}: ${err.message}`);
+					return;
+				}
+				console.log(
+					`> ${basename}: ${data.State}, ${data.LastUpdateStatus}, ${data.LastUpdateStatusReasonCode}, ${data.CodeSize} bytes`
+				);
 			}
-			console.log(`> ${basename}: ${data.State}, ${data.LastUpdateStatus}, ${data.LastUpdateStatusReasonCode}, ${data.CodeSize} bytes`);
-		});
-	})
+		);
+	});
 }
-
