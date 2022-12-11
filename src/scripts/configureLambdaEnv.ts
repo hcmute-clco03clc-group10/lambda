57bf8fe058660b97c061699493f4c04b47890331
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-12 05:10:08 +0700

    Remove brackets from function name.

--- src/scripts/configureLambdaEnv.ts
+++ src/scripts/configureLambdaEnv.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 const lambda = new AWS.Lambda({
 	region: 'us-east-1',
-	apiVersion: 'latest'
+	apiVersion: 'latest',
 });
 const makeSecret = () => crypto.randomBytes(256).toString('base64');
 const JWT_ACCESS_SECRET = makeSecret();
@@ -14,22 +14,26 @@ const JWT_REFRESH_SECRET = makeSecret();
 console.log(`> make access secret: ${JWT_ACCESS_SECRET}`);
 console.log(`> make refresh secret: ${JWT_REFRESH_SECRET}`);
 
-for(const dir of globDirs('dist/functions')) {
+for (const dir of globDirs('dist/functions')) {
 	const basename = path.basename(dir);
 	console.log(`> configure ${basename} environment variables`);
-	lambda.updateFunctionConfiguration({
-		FunctionName: basename,
-		Environment: {
-			Variables: {
-				JWT_ACCESS_SECRET,
-				JWT_REFRESH_SECRET,
+	lambda.updateFunctionConfiguration(
+		{
+			FunctionName: basename.replace('{', '').replace('}', ''),
+			Environment: {
+				Variables: {
+					JWT_ACCESS_SECRET,
+					JWT_REFRESH_SECRET,
+				},
+			},
+		},
+		(err) => {
+			if (err) {
+				console.log(
+					`> ${basename} failed to configure - ${err.message}`
+				);
+				return;
 			}
 		}
-	}, (err) => {
-		if (err) {
-			console.log(`> ${basename} failed to configure - ${err.message}`);
-			return;
-		}
-	});
+	);
 }
-
